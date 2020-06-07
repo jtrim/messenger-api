95db51bdd107ae59714b9a79b6899fb8c031668c
Jesse Trimble <jesseltrimble@gmail.com>
2020-06-06 21:20:49 -0600

    Base app config

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,23 @@ module MessengerApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.api_only = true
+    config.debug_exception_response_format = :api
+    config.time_zone = 'America/Denver'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators do |g|
+      g.orm :active_record
+      g.test_framework :rspec
+      g.template_engine nil
+      g.assets false
+      g.helper false
+      g.integration_tool :capybara
+      g.system_tests :rspec
+      g.stylesheets false
+    end
   end
 end
